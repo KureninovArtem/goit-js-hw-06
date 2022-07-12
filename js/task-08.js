72ddec385f4c57dead489e23051f80dc71299237
KureninovArtem <97453414+KureninovArtem@users.noreply.github.com>
2022-07-12 20:16:15 +0300

    FinishTask-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("Пожалуйста, заполните все поля!");
+        event.currentTarget.reset();
+        return;
+    }
+
+    const dataForm = {
+        email: email.value,
+        password: password.value
+    }
+    
+    console.log(dataForm);
+    event.currentTarget.reset();
+};
